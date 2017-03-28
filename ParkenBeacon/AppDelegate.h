cedfae25f3f5957f93dbe98f79608bb0352f42a6
Narcis Zait <narcis.zait@gmail.com>
2017-03-29 01:36:26 +0800

    Initial Commit

---
+++ ParkenBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParkenBeacon
+//
+//  Created by Narcis Zait on 29/03/2017.
+//  Copyright © 2017 Narcis Zait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParkenBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParkenBeacon
+//
+//  Created by Narcis Zait on 29/03/2017.
+//  Copyright © 2017 Narcis Zait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
